570ea851fa37ff2ae5029dd6f5eeddb3a2fc4fcb
Hebert <hebertphp@gmail.com>
2021-09-16 17:45:33 -0300

    16/09

---
+++ Projeto_video_background/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+
+    const menuToggle = document.querySelector('.toggle');
+    const showcase = document.querySelector('.showcase');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        showcase.classList.toggle('active');
+    })
+})
